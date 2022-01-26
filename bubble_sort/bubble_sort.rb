4d08f5a69485cd2c5279630a05d5f26c91854b14
Philipe <philipe-leandro@hotmail.com>
2022-01-26 08:52:33 -0300

    Add bubble search bubble sort

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,17 @@
+def bubble_sort(array)
+  array_size = array.length
+
+  loop do 
+    condition = false
+
+    (array_size-1).times do |i|
+      if array[i] > array[i+1]
+        array[i],array[i+1] = array[i+1],array[i]
+        condition = true
+      end
+    end
+
+    break if not condition
+  end
+   puts "#{array}"
+end
